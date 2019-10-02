6ddfd177df048f127e4cccd983d96e947a19e1ba
dddddppppp <56067526+dddddppppp@users.noreply.github.com>
2019-10-02 11:31:29 +0200

    homer

---
+++ animations/dddddpppppppp/script.js
@@ -0,0 +1,15 @@
+var el = document.getElementById('homer');
+
+function cloneHomer() {
+	var i = Math.floor(Math.random() * 30000)
+	let clone = el.cloneNode( true );
+
+	document.querySelector('body').appendChild( clone )
+	clone.setAttribute( 'id', 'homer' + i);
+	var _ = document.getElementById('homer' + i);
+	_.style.position = 'absolute';
+	_.style.top = Math.floor(Math.random() * 100)+ '%';
+	_.style.left = Math.floor(Math.random() * 100) + '%';
+}
+
+setInterval(cloneHomer, 1000)
